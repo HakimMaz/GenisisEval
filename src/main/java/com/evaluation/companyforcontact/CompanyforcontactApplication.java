f693c692681a24d7cf3afa7ad82b7c7d52f32ff0
HakimMaz <maz.hakim93@gmail.com>
2023-04-18 15:07:30 +0200

    Feature -> init gradle project

---
+++ src/main/java/com/evaluation/companyforcontact/CompanyforcontactApplication.java
@@ -0,0 +1,13 @@
+package com.evaluation.companyforcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyforcontactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyforcontactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluation/companyforcontact/CompanyforcontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluation.companyforcontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyforcontactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
