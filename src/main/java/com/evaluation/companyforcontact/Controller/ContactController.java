51116ffe682456d3a695fb0056843836d5ee2d92
HakimMaz <maz.hakim93@gmail.com>
2023-04-20 13:56:42 +0200

    Feature ->Adding Contact Controller

--- src/main/java/com/evaluation/companyforcontact/Controller/ContactController.java
+++ src/main/java/com/evaluation/companyforcontact/Controller/ContactController.java
@@ -1,12 +1,14 @@
 package com.evaluation.companyforcontact.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ContactController {
-    @GetMapping("/hello")
-    public String sayHello(){
-        return " hello spring";
-    }
+   @PostMapping("/create")
+    public ResponseEntity<Contact> create(){
+       return contactRepository.save();
+   }
 }
