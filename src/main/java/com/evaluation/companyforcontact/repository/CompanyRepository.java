6545e375da539e81a7929cbbcaf76a8d2bc78ed3
HakimMaz <maz.hakim93@gmail.com>
2023-05-08 14:44:30 +0200

    Feature ->Adding company repository

---
+++ src/main/java/com/evaluation/companyforcontact/repository/CompanyRepository.java
@@ -0,0 +1,14 @@
+package com.evaluation.companyforcontact.repository;
+
+import com.evaluation.companyforcontact.model.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface CompanyRepository extends JpaRepository<Company,Long> {
+
+    public Company findByTvaNumber(Long tva);
+    void deleteByUuid(UUID uuid);
+
+}
